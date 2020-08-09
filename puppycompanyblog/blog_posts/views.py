985c02a0aa9f26d98b823efdc5d4a184883af8a4
Jonathan Le <jonathan@Jonathans-iMac.local>
2020-08-09 11:39:55 -0700

    [LRN-12] Final Project!

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -43,7 +43,7 @@ def blog_post(blog_post_id):
 # -----------------------------------------------------------
 # UPDATE A BLOG POST
 # -----------------------------------------------------------
-@blog_posts.route('<int:blog_post_id>/update', methods=['GET', 'POST'])
+@blog_posts.route('/<int:blog_post_id>/update', methods=['GET', 'POST'])
 @login_required
 def update(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -64,7 +64,7 @@ def update(blog_post_id):
         return redirect(url_for('blog_posts.blog_post', blog_post_id = blog_post.id))
 
     # On first update form view, the fields should have current text and title prefilled
-    elif request.method = 'GET':
+    elif request.method == 'GET':
         form.title.data = blog_post.title
         form.text.data = blog_post.text
 
@@ -76,7 +76,7 @@ def update(blog_post_id):
 # DELETE A BLOG POST
 # this won't have a template, but we'll use a Bootstrap modal
 # -----------------------------------------------------------
-@blog_posts.route('<int:blog_post_id>/delete', methods=['GET', 'POST'])
+@blog_posts.route('/<int:blog_post_id>/delete', methods=['GET', 'POST'])
 @login_required
 def delete_post(blog_post_id):
 
